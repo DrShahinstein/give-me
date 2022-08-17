ba6afebac7652208e94f1e3c9ec3b802ddfd26a0
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-08-17 15:29:22 +0300

    Typos & Complete TODOs

--- cboard/__init__.py
+++ cboard/__init__.py
@@ -1,5 +1,6 @@
 import click
 import pyperclip as clip
+from .representations import create_new, get
 
 
 @click.group()
@@ -7,26 +8,26 @@ def cli(): pass
 
 
 @cli.command()
-@click.argument("name")
-def copy(name):
+@click.argument("representing_name")
+def copy(representing_name):
     """Copy something you want to the clipboard."""
 
-    # TODO: Get the name and copy it to the clipboard.
-    clip.copy()
+    content = get(representing_name)
+    clip.copy(content)
 
-    click.echo(f"{name} copied!")
+    click.echo(f"{representing_name} copied!")
 
 
 @cli.command()
-@click.argument("name")
-def create(name):
+@click.argument("representing_name")
+def create(representing_name):
     """Create a new representation representing what you are going to copy."""
 
-    text = click.prompt(f"What do you want {name} to represent?")
-    # TODO: Create a new representation.
+    text = click.prompt(f"What do you want {representing_name} to represent?")
+    create_new(representing_name, text)
 
-    click.echo(f"`{name}` created successfully!")
-    click.echo(f"You can now `cboard copy {name}` it.")
+    click.echo(f"`{representing_name}` created successfully!")
+    click.echo(f"You can now `cboard copy {representing_name}` it.")
 
 
 def main():
