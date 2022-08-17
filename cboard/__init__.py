f13505bd09167e039fcaffd306126ac76a386f3a
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-08-17 16:09:49 +0300

    New command: Remove

--- cboard/__init__.py
+++ cboard/__init__.py
@@ -1,6 +1,6 @@
 import click
 import pyperclip as clip
-from .representations import create_new, get
+from .representations import create_new, get, remove
 
 
 @click.group()
@@ -23,11 +23,21 @@ def copy(representing_name):
 def create(representing_name):
     """Create a new representation representing what you are going to copy."""
 
-    text = click.prompt(f"What do you want {representing_name} to represent?")
-    create_new(representing_name, text)
+    content = click.prompt(f"What do you want {representing_name} to represent?")
+    create_new(representing_name, content)
 
     click.echo(f"`{representing_name}` created successfully!")
     click.echo(f"You can now `cboard copy {representing_name}` it.")
+
+
+@cli.command()
+@click.argument("representing_name")
+def remove(representing_name):
+    """Delete a representation representing what you are going to copy."""
+
+    remove(representing_name)
+
+    click.echo(f"`{representing_name}` removed successfully!")
 
 
 def main():
