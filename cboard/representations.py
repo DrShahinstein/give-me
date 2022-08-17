a6b5277a13f7b8bd18d336e8d7c153dbb1a29b80
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-08-17 15:28:53 +0300

    Be done with functs providing file operations

--- cboard/representations.py
+++ cboard/representations.py
@@ -11,6 +11,35 @@ import json
 import os
 from pathlib import Path
 
-DEFAULT_DIR = Path(os.environ['HOME']) / ".representations"
+ROOT_DIR = Path(os.environ['HOME']) / ".representations"
+JSON_PATH = ROOT_DIR / "index.json"
 
-# TODO
+
+def create_json(path=JSON_PATH):
+    with open(path, "w") as f:
+        f.write("{}")
+
+
+def create_new(representing_name, password):
+    dir_exists = ROOT_DIR.exists()
+    json_exists = JSON_PATH.exists()
+    new_data = {representing_name: password, }
+
+    if not dir_exists:
+        os.mkdir(ROOT_DIR)
+        create_json()
+
+    if not json_exists:
+        create_json()
+
+    with open(JSON_PATH, "r+", encoding="utf-8") as f:
+        content = json.load(f)
+        content.update(new_data)
+        f.seek(0)
+        json.dump(content, f, indent=4)
+
+
+def get(representing_name):
+    with open(JSON_PATH, "r") as f:
+        content = json.load(f)
+        return content[representing_name]
