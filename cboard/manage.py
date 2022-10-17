baebaf7900bc23787feb9a2e65185bb042b97ea7
1TaylanOzturk <1taylanozturk@protonmail.com>
2022-10-17 20:24:21 +0300

    Change variable names

--- cboard/manage.py
+++ cboard/manage.py
@@ -15,7 +15,7 @@ from .utils import pop, get_dict_from_json, write_to_json, create_json, ROOT_DIR
 def create_new(representing_name, content):
     dir_exists = ROOT_DIR.exists()
     json_exists = JSON_PATH.exists()
-    new_representation = {representing_name: content, }
+    pair = {representing_name: content, }
 
     if not dir_exists:
         os.mkdir(ROOT_DIR)
@@ -24,7 +24,7 @@ def create_new(representing_name, content):
     if not json_exists:
         create_json()
 
-    write_to_json(new_representation)
+    write_to_json(pair)
 
 
 def delete(representing_name):
