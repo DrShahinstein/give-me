23fe311cbb1b90e3bb9f9b8b0b7fd51914f9319b
DrShahinstein <1taylanozturk@protonmail.com>
2024-07-26 23:30:10 +0300

    Create help.py

---
+++ give_me/src/help.py
@@ -0,0 +1,12 @@
+HELP_TEXT = """\
+Usage: give_me <command_or_password_name>
+
+Commands:
+  create   Create new password
+  edit     Edit existing password
+  remove   Remove password
+  list     List all passwords
+ 
+Options:
+  --no-private   Show passwords in plain text (while listing)
+"""
